cfcc9a96f389b50a75fa92a90b415c0f1bf79733
nbcchris <chris.poole@qa.com>
2015-11-05 14:54:20 +0000

    Beginning Login

--- target/classes/main/java/GUImain.scala
+++ target/classes/main/java/GUImain.scala
@@ -2,38 +2,62 @@
 import scalafx.Includes._
 import scalafx.scene.Scene
 import scalafx.application.JFXApp
-import scalafx.scene.control.Button
+import scalafx.scene.control.{Button, TextField}
 import scalafx.scene.layout.VBox
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.paint.Color._
 import scalafx.geometry.Insets
+import scalafx.scene.paint.LinearGradient
+import scalafx.scene.paint.Stops
+import scalafx.scene.layout.HBox
+import scalafx.scene.image.Image
+import javafx.scene.shape.Rectangle
+import javafx.scene.paint.ImagePattern
 
 object GUImain extends JFXApp {
-  
+    
   stage = new PrimaryStage {
     title = "Test"
+    
+    def createRect(): Rectangle ={
+      val image = new Image("file:src/images/logo-white.png")
+      val rect = new Rectangle(0,0,90,90)
+      rect setFill(new ImagePattern(image))
+      rect
+    }
+    
+    val pic = createRect
+    
     scene = new Scene{  
-      fill = Black
-      content = new VBox {
+      fill = new LinearGradient(endX = 0,stops = Stops(SeaGreen, PaleGreen))
+         
+      content = new VBox{
         padding = Insets(200)
-        content = Seq(
-           new Button{
-             text = "Hello?"
-             style = "-fx-font-size: 48pt"
-             onAction = handle{
-               println("Hello World") 
+        children = Seq(new TextField
+                      ,new HBox {
+          children = Seq(
+            new Button{
+              text = "Log In"
+              padding=Insets(10)
+              style = "-fx-font-size: 16pt"
+              onAction = handle{
+                println("Log In") 
+              }
+            },
+            new Button{
+              text = "Exit"
+              style = "-fx-font-size: 16pt"
+              padding=Insets(10)
+              onAction = handle{
+                System exit(0)
+              }
             }
-           },
-          new Button{
-             text = "Bye?"
-             style = "-fx-font-size: 48pt"
-             onAction = handle{
-               println("Goodbye World") 
-            }
-          }
+          )
+        }
         )
-     }
-    }  
+      }
+      content += pic
+    }
   }
 }
